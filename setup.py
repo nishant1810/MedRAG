129423039a0687c2870d8b35ea601800652e5735
Nishant1810 <gaur18nishant@gmail.com>
2026-06-09 15:59:43 +0530

    Updates

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name= "Medical-Chatbot",
     version= "0.1.0",
-    author= "Chitransh Kumar",
-    author_email= "chitranshhere11@gmail.com",
+    author= "Nishant Gaur",
+    author_email= "gaur18nishant@gmail.com",
     packages= find_packages(),
     install_requires= []
 )
